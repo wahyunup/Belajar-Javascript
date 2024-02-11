aaee093247493028675131c090baafa29432e53d
wahyunup <wahyunurpambuko@gmail.com>
2024-02-11 18:29:24 +0700

    add nested loop

--- nested loop/app.js
+++ nested loop/app.js
@@ -7,9 +7,17 @@
 // [x[2],x[0]]=[x[0],x[2]];
 // console.log(x);
 
-function calculateBill(meal, taxRate = 0.05){
-    const total = meal * (1 + taxRate);
-    return total;
-}
+// function calculateBill(meal, taxRate = 0.05){
+//     const total = meal * (1 + taxRate);
+//     return total;
+// }
 
-const myTotal = calculateBill (100, 0.13);
+// const myTotal = calculateBill (100, 0.13);
+
+const pilihan = 'abcd'
+for (let i = 1; i <= 10; i++){
+    console.log(`${i}. soal nomor ${i}:`);
+    for (let j = 0; j < pilihan.length; j++){
+        console.log(`${pilihan[j]}. pilihan jawaban`);
+    }
+}
